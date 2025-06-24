11dbaf466a9b624ec054ff0fe2965ab1f0d253fe
Jason Codiot <jc2552@njit.edu>
2025-06-23 20:11:36 -0400

    Add operations_map to __init__.py for strategy pattern

--- app/operations/__init__.py
+++ app/operations/__init__.py
@@ -1,15 +1,12 @@
-from .add_strategy import AddStrategy
-from .subtract import SubtractStrategy
-from .multiply import MultiplyStrategy
-from .divide import DivideStrategy
-from .power import PowerStrategy
-from .root import RootStrategy
+from app.operations.add_strategy import AddStrategy
+from app.operations.subtract_strategy import SubtractStrategy
+from app.operations.multiply_strategy import MultiplyStrategy
+from app.operations.divide_strategy import DivideStrategy
 
 operations_map = {
-    'add': AddStrategy,
-    'subtract': SubtractStrategy,
-    'multiply': MultiplyStrategy,
-    'divide': DivideStrategy,
-    'power': PowerStrategy,
-    'root': RootStrategy
+    "add": lambda a, b: AddStrategy().calculate(a, b),
+    "subtract": lambda a, b: SubtractStrategy().calculate(a, b),
+    "multiply": lambda a, b: MultiplyStrategy().calculate(a, b),
+    "divide": lambda a, b: DivideStrategy().calculate(a, b),
 }
+
