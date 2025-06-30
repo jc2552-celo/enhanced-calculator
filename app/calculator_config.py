f24efe32ff09e6879e3fa0ccf2d745a214f56577
Jason Codiot <jc2552@njit.edu>
2025-06-30 09:40:49 -0400

    Define HISTORY_FILE constant to match test expectations

--- app/calculator_config.py
+++ app/calculator_config.py
@@ -3,7 +3,8 @@ import os
 
 load_dotenv()
 
-HISTORY_FILE = os.getenv("HISTORY_FILE", "data/history.csv")  # <- Match test expectation
+# Define this constant to match the test expectation
+HISTORY_FILE = os.getenv("HISTORY_FILE", "data/history.csv")
 
 def get_history_file():
     return HISTORY_FILE
